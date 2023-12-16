ffca816642548f2aaea286a1b97f8bf241172c2d
Bhargavoza1 <66210089+Bhargavoza1@users.noreply.github.com>
2023-12-16 16:44:48 +0530

    Update perceptron.py

--- perceptron.py
+++ perceptron.py
@@ -1,13 +1,26 @@
 import numpy as np
 
-inputs = np.array([-4,0,2,-7])
+
+inputs = np.array([0,0,2,3])
 weights = np.array([0.1,0.5,0.7,1.2])
+bias = -6
 
-weightsum= np.dot(inputs,weights)
+weightsum= np.dot(inputs,weights) + bias
 
+def step_function(weightsum):
+    if(weightsum >= 0):
+        return 1
+    else:
+        return 0
+
+step_function(weightsum)
 
 # weightsum= dot(x,w)
 
-#16 =  dot([5,7,2,9] , [0.1,0.5,0.7,1.2])
+bias = -6
+# -1 =  dot([0,0,2,3] , [0.1,0.5,0.7,1.2]) + bias
+# 0 = step_function(-1)
 
-#-7.4 =  dot([-4,0,2,-7] , [0.1,0.5,0.7,1.2])
+# bias = 6
+# 1 =  dot([-4,0,2,-5] , [0.1,0.5,0.7,1.2]) + bias
+# 1 = step_function(1)
