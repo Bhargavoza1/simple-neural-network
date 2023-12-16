01d2fe494c04e0a95c2529e4a2b80d32289f41bb
Bhargavoza1 <66210089+Bhargavoza1@users.noreply.github.com>
2023-12-16 14:48:11 +0530

    weightsum

---
+++ perceptron.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+inputs = np.array([-4,0,2,-7])
+weights = np.array([0.1,0.5,0.7,1.2])
+
+weightsum= np.dot(inputs,weights)
+
+
+# weightsum= dot(x,w)
+
+#16 =  dot([5,7,2,9] , [0.1,0.5,0.7,1.2])
+
+#-7.4 =  dot([-4,0,2,-7] , [0.1,0.5,0.7,1.2])
