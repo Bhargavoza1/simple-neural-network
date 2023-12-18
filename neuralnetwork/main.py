8e883a3e52e3a994d76076facde981675cb0a77b
Bhargavoza1 <66210089+Bhargavoza1@users.noreply.github.com>
2023-12-18 13:17:08 +0530

    Update main.py

--- neuralnetwork/main.py
+++ neuralnetwork/main.py
@@ -2,33 +2,48 @@ import numpy as np
 from module import Module
 from linear import Linear
 from activation import ReLU,Sigmoid
+
+x_train = np.array([[[0,0]], [[0,1]], [[1,0]], [[1,1]]])
+y_train = np.array([[[0]], [[1]], [[1]], [[0]]])
+
 class MLP(Module):
     # define model elements
     def __init__(self, n_inputs):
-        super(MLP, self).__init__()
+
         # input to first hidden layer
-        self.hidden1 = Linear(n_inputs, 10)
+        self.hidden1 = Linear(n_inputs, 3)
         self.act1 = ReLU()
         # second hidden layer
-        self.hidden2 = Linear(10, 8)
+        self.hidden2 = Linear(3, 2)
         self.act2 = ReLU()
         # third hidden layer and output
-        self.hidden3 = Linear(8, 1)
+        self.hidden3 = Linear(2, 1)
         self.act3 = Sigmoid()
 
     # forward propagate input
     def forward(self, X):
         # input to first hidden layer
-        X = self.hidden1(X)
-        X = self.act1(X)
+        X = self.hidden1.forward(X)
+        X = self.act1.forward(X)
         # second hidden layer
-        X = self.hidden2(X)
-        X = self.act2(X)
+        X = self.hidden2.forward(X)
+        X = self.act2.forward(X)
         # third hidden layer and output
-        X = self.hidden3(X)
-        X = self.act3(X)
+        X = self.hidden3.forward(X)
+        X = self.act3.forward(X)
         return X
 
+def predict(row, model):
+
+    # make prediction
+    yhat = model.forward(row)
+
+    return yhat
 
 
- 
+
+model = MLP(2)
+
+yhat = predict(x_train, model)
+
+print(yhat)
